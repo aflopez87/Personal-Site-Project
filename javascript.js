971982db11b9f757aa0c38213c4b01461074056f
Alexis <aflopez87@gmail.com>
2025-04-15 20:33:51 -0400

    Additional Changes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+// This is a JavaScript file for the personal website
+console.log('Hello World!')
+
+// This is part of the form submission process for the form page
 const form = document.querySelector('#form')
 const submitButton = document.querySelector('#submit')
 
--- success.js
+++ success.js
@@ -1,3 +1,7 @@
+// This is a JavaScript file for the personal website
+console.log('Hello World!')
+
+// This is part of the form submission process for the success page
 setTimeout(() => {
     window.location.href = window.location.origin
   }, 5000)
